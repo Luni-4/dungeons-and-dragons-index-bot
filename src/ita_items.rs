3e6ce7dd405a51df9e696185fec2846361a9590d
Luni-4 <luni-4@hotmail.it>
2020-10-14 13:16:28 +0200

    Sort out maps keys

--- src/ita_items.rs
+++ src/ita_items.rs
@@ -1,29 +1,38 @@
 // This file is auto-generated. DO NOT EDIT!!!
 
-const ONORE: phf::Set<&'static str> = ::phf::Set {
-    map: ::phf::Map {
-        key: 3213172566270843353,
-        disps: ::phf::Slice::Static(&[(1, 0)]),
-        entries: ::phf::Slice::Static(&[
-            ("Manuale del Giocatore ---> pages 270, 271\n", ()),
-            ("Manuale del Master ---> pages 260, 261\n", ()),
-        ]),
-    },
-};
-
-const SANITA: phf::Set<&'static str> = ::phf::Set {
-    map: ::phf::Map {
-        key: 3213172566270843353,
-        disps: ::phf::Slice::Static(&[(0, 0)]),
-        entries: ::phf::Slice::Static(&[
-            ("Manuale del Master ---> pages 220, 250\n", ()),
-            ("Manuale del Giocatore ---> pages 230, 251\n", ()),
-        ]),
-    },
-};
-
-pub static ITA_MAP: phf::Map<&'static str, phf::Set<&'static str>> = ::phf::Map {
+const ONORE: phf::Set<&'static str> =
+::phf::Set { map: ::phf::Map {
     key: 3213172566270843353,
-    disps: ::phf::Slice::Static(&[(0, 0)]),
-    entries: ::phf::Slice::Static(&[("sanità", SANITA), ("onore", ONORE)]),
+    disps: ::phf::Slice::Static(&[
+        (1, 0),
+    ]),
+    entries: ::phf::Slice::Static(&[
+        ("Manuale del Giocatore ---> pages 270, 271\n", ()),
+        ("Manuale del Master ---> pages 260, 261\n", ()),
+    ]),
+} };
+
+const SANITA: phf::Set<&'static str> =
+::phf::Set { map: ::phf::Map {
+    key: 3213172566270843353,
+    disps: ::phf::Slice::Static(&[
+        (0, 0),
+    ]),
+    entries: ::phf::Slice::Static(&[
+        ("Manuale del Master ---> pages 220, 250\n", ()),
+        ("Manuale del Giocatore ---> pages 230, 251\n", ()),
+    ]),
+} };
+
+pub static ITA_MAP: phf::Map<&'static str, phf::Set<&'static str>> =
+::phf::Map {
+    key: 3213172566270843353,
+    disps: ::phf::Slice::Static(&[
+        (0, 0),
+    ]),
+    entries: ::phf::Slice::Static(&[
+        ("sanità", SANITA),
+        ("onore", ONORE),
+    ]),
 };
+
