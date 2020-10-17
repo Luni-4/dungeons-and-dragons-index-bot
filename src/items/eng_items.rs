71bbb098e7415d140d31fb09f675ecac3001f1e3
Luni-4 <luni-4@hotmail.it>
2020-10-17 21:33:00 +0200

    Add more English items

--- src/items/eng_items.rs
+++ src/items/eng_items.rs
@@ -1,13 +1,57 @@
 // This file is auto-generated. DO NOT EDIT!!!
 
-const HONOUR: phf::Set<&'static str> = ::phf::Set {
+const CHASE: phf::Set<&'static str> = ::phf::Set {
     map: ::phf::Map {
         key: 3213172566270843353,
-        disps: ::phf::Slice::Static(&[(1, 0)]),
-        entries: ::phf::Slice::Static(&[
-            ("Dungeon\'s Guide ---> pages 260, 261\n", ()),
-            ("Player\'s Handbook ---> pages 270, 271\n", ()),
-        ]),
+        disps: ::phf::Slice::Static(&[(0, 0)]),
+        entries: ::phf::Slice::Static(&[(
+            "Dungeon\'s Guide ---> pages 252\n",
+            (),
+        )]),
+    },
+};
+
+const HONOR: phf::Set<&'static str> = ::phf::Set {
+    map: ::phf::Map {
+        key: 3213172566270843353,
+        disps: ::phf::Slice::Static(&[(0, 0)]),
+        entries: ::phf::Slice::Static(&[(
+            "Dungeon\'s Guide ---> pages 264\n",
+            (),
+        )]),
+    },
+};
+
+const KITS: phf::Set<&'static str> = ::phf::Set {
+    map: ::phf::Map {
+        key: 3213172566270843353,
+        disps: ::phf::Slice::Static(&[(0, 0)]),
+        entries: ::phf::Slice::Static(&[(
+            "Player\'s Handbook ---> pages 255\n",
+            (),
+        )]),
+    },
+};
+
+const MADNESS: phf::Set<&'static str> = ::phf::Set {
+    map: ::phf::Map {
+        key: 3213172566270843353,
+        disps: ::phf::Slice::Static(&[(0, 0)]),
+        entries: ::phf::Slice::Static(&[(
+            "Dungeon\'s Guide ---> pages 258\n",
+            (),
+        )]),
+    },
+};
+
+const POISONS: phf::Set<&'static str> = ::phf::Set {
+    map: ::phf::Map {
+        key: 3213172566270843353,
+        disps: ::phf::Slice::Static(&[(0, 0)]),
+        entries: ::phf::Slice::Static(&[(
+            "Dungeon\'s Guide ---> pages 257\n",
+            (),
+        )]),
     },
 };
 
@@ -15,19 +59,23 @@ const SANITY: phf::Set<&'static str> = ::phf::Set {
     map: ::phf::Map {
         key: 3213172566270843353,
         disps: ::phf::Slice::Static(&[(0, 0)]),
-        entries: ::phf::Slice::Static(&[
-            ("Dungeon\'s Guide ---> pages 220, 250\n", ()),
-            ("Player\'s Handbook ---> pages 230, 251\n", ()),
-        ]),
+        entries: ::phf::Slice::Static(&[(
+            "Dungeon\'s Guide ---> pages 264\n",
+            (),
+        )]),
     },
 };
 
 pub static ENG_MAP: phf::Map<&'static str, phf::Set<&'static str>> =
     ::phf::Map {
         key: 3213172566270843353,
-        disps: ::phf::Slice::Static(&[(1, 0)]),
+        disps: ::phf::Slice::Static(&[(0, 0), (0, 0)]),
         entries: ::phf::Slice::Static(&[
-            ("honour", HONOUR),
+            ("madness", MADNESS),
+            ("honor", HONOR),
             ("sanity", SANITY),
+            ("kits", KITS),
+            ("poisons", POISONS),
+            ("chase", CHASE),
         ]),
     };
