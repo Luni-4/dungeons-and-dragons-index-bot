d134a5c918e830ee97add722c878adaf4a79a81c
Luni-4 <luni-4@hotmail.it>
2020-10-17 22:14:34 +0200

    Add more Italian items

--- src/items/ita_items.rs
+++ src/items/ita_items.rs
@@ -1,13 +1,57 @@
 // This file is auto-generated. DO NOT EDIT!!!
 
+const INSEGUIMENTI: phf::Set<&'static str> = ::phf::Set {
+    map: ::phf::Map {
+        key: 3213172566270843353,
+        disps: ::phf::Slice::Static(&[(0, 0)]),
+        entries: ::phf::Slice::Static(&[(
+            "Manuale del Master ---> pages 252\n",
+            (),
+        )]),
+    },
+};
+
 const ONORE: phf::Set<&'static str> = ::phf::Set {
     map: ::phf::Map {
         key: 3213172566270843353,
-        disps: ::phf::Slice::Static(&[(1, 0)]),
-        entries: ::phf::Slice::Static(&[
-            ("Manuale del Giocatore ---> pages 270, 271\n", ()),
-            ("Manuale del Master ---> pages 260, 261\n", ()),
-        ]),
+        disps: ::phf::Slice::Static(&[(0, 0)]),
+        entries: ::phf::Slice::Static(&[(
+            "Manuale del Master ---> pages 264\n",
+            (),
+        )]),
+    },
+};
+
+const KITS: phf::Set<&'static str> = ::phf::Set {
+    map: ::phf::Map {
+        key: 3213172566270843353,
+        disps: ::phf::Slice::Static(&[(0, 0)]),
+        entries: ::phf::Slice::Static(&[(
+            "Manuale del Giocatore ---> pages 154\n",
+            (),
+        )]),
+    },
+};
+
+const FOLLIA: phf::Set<&'static str> = ::phf::Set {
+    map: ::phf::Map {
+        key: 3213172566270843353,
+        disps: ::phf::Slice::Static(&[(0, 0)]),
+        entries: ::phf::Slice::Static(&[(
+            "Manuale del Master ---> pages 258\n",
+            (),
+        )]),
+    },
+};
+
+const VELENI: phf::Set<&'static str> = ::phf::Set {
+    map: ::phf::Map {
+        key: 3213172566270843353,
+        disps: ::phf::Slice::Static(&[(0, 0)]),
+        entries: ::phf::Slice::Static(&[(
+            "Manuale del Master ---> pages 257\n",
+            (),
+        )]),
     },
 };
 
@@ -15,16 +59,23 @@ const SANITA: phf::Set<&'static str> = ::phf::Set {
     map: ::phf::Map {
         key: 3213172566270843353,
         disps: ::phf::Slice::Static(&[(0, 0)]),
-        entries: ::phf::Slice::Static(&[
-            ("Manuale del Master ---> pages 220, 250\n", ()),
-            ("Manuale del Giocatore ---> pages 230, 251\n", ()),
-        ]),
+        entries: ::phf::Slice::Static(&[(
+            "Manuale del Master ---> pages 264\n",
+            (),
+        )]),
     },
 };
 
 pub static ITA_MAP: phf::Map<&'static str, phf::Set<&'static str>> =
     ::phf::Map {
         key: 3213172566270843353,
-        disps: ::phf::Slice::Static(&[(0, 0)]),
-        entries: ::phf::Slice::Static(&[("sanità", SANITA), ("onore", ONORE)]),
+        disps: ::phf::Slice::Static(&[(1, 5), (0, 0)]),
+        entries: ::phf::Slice::Static(&[
+            ("sanità", SANITA),
+            ("onore", ONORE),
+            ("veleni", VELENI),
+            ("kits", KITS),
+            ("inseguimenti", INSEGUIMENTI),
+            ("follia", FOLLIA),
+        ]),
     };
